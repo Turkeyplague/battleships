b19afd991657ce8e655118bc053b310e237f2136
Andrew Hill <turkeyplague@hotmail.com>
2015-09-18 13:54:48 +1000

    Split project into VB version and CS version.

---
+++ cs/src/GameLogic.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SwinGameSDK;
+
+using static UtilityFunctions;
+using static GameResources;
+using static DeploymentController;
+using static DiscoveryController;
+using static EndingGameController;
+using static MenuController;
+using static HighScoreController;
+
+static class GameLogic
+
+{
+    public static void Main()
+    {
+        //Opens a new Graphics Window
+        SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+        //Load Resources
+        GameResources.LoadResources();
+
+        SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+
+        //Game Loop
+        do {
+            GameController.HandleUserInput();
+            GameController.DrawScreen();
+        } while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
+
+        SwinGame.StopMusic();
+
+        //Free Resources and Close Audio, to end the program.
+        GameResources.FreeResources();
+    }
+}
